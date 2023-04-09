8fde3cb06ac667ab290d01622f123eedece2747a
Sean Sunoo <97641529+Ssunoo2@users.noreply.github.com>
2023-04-08 15:12:19 -1000

    d

--- app/imports/ui/pages/AddClub.jsx
+++ app/imports/ui/pages/AddClub.jsx
@@ -1,39 +1,25 @@
 import React from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
-import { AutoForm, ErrorsField, NumField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
-import swal from 'sweetalert';
-import { Meteor } from 'meteor/meteor';
-import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
-import SimpleSchema from 'simpl-schema';
-import { Stuffs } from '../../api/stuff/Stuff';
+import { ErrorsField, NumField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
+
 // Create a schema to specify the structure of the data to appear in the form.
 /* Renders the AddStuff page for adding a document. */
-const AddStuff = () => {
-
-  // On submit, insert the data.
-
-  // Render the form. Use Uniforms: https://github.com/vazco/uniforms
-  let fRef = null;
-  return (
-    <Container className="py-3">
-      <Row className="justify-content-center">
-        <Col xs={5}>
-          <Col className="text-center"><h2>Add Stuff</h2></Col>
-          <AutoForm ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => submit(data, fRef)}>
-            <Card>
-              <Card.Body>
-                <TextField name="name" />
-                <NumField name="quantity" decimal={null} />
-                <SelectField name="condition" />
-                <SubmitField value="Submit" />
-                <ErrorsField />
-              </Card.Body>
-            </Card>
-          </AutoForm>
-        </Col>
-      </Row>
-    </Container>
-  );
-};
-
-export default AddStuff;
+const AddClub = () => (
+  <Container className="py-3">
+    <Row className="justify-content-center">
+      <Col xs={5}>
+        <Col className="text-center"><h2>Create a New Club</h2></Col>
+        <Card>
+          <Card.Body>
+            <TextField name="name" />
+            <NumField name="quantity" decimal={null} />
+            <SelectField name="condition" />
+            <SubmitField value="Submit" />
+            <ErrorsField />
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  </Container>
+);
+export default AddClub;

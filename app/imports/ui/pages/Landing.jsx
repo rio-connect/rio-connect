27299af4bacfe82bfe57c9f0f123766b50341713
Sean Sunoo <97641529+Ssunoo2@users.noreply.github.com>
2023-04-08 14:33:12 -1000

    update centering

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Container, Button, Row } from 'react-bootstrap';
+import { Container, Button, Row, Image } from 'react-bootstrap';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
 
   <Container className="pt-5 d-flex justify-content-center">
     <Row>
-      <div className="justify-content-center">
-        <img className="py-5" id="centerImage" src="images/manoa-students.jpeg" alt="UH Manoa students." width="90%" />
+      <div className="justify-content-center align-content-center">
+        <Image className="py-5 rounded mx-auto d-block" id="centerImage" src="images/manoa-students.jpeg" alt="UH Manoa students." width="90%" />
       </div>
       <div>
 
-        <h1>Welcome to Rio-Connect</h1>
-        <p>Rio-Connect is the ultimate app for college students looking to discover new clubs and organizations on their university campus!
+        <h1 className="text-center pb-3">Welcome to Rio-Connect!</h1>
+        <p>
+          Rio-Connect is the ultimate app for college students looking to discover new clubs and organizations on their university campus!
         </p>
         <p>
           Are you tired of attending the same events and meetings week after week? Do you want to branch out and explore all that your university has to offer? Look no further than Rio-Connect. Our app is designed to connect you with new

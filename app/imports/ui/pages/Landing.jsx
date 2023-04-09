d82b2b0786c4de7a195c4a47f1bde7f8bf508d38
Sean Sunoo <97641529+Ssunoo2@users.noreply.github.com>
2023-04-08 14:24:01 -1000

    put brief generic description + get started button

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,24 +5,33 @@ import { Container, Button, Row } from 'react-bootstrap';
 const Landing = () => (
 
   <Container className="pt-5 d-flex justify-content-center">
-    <div>
-      <img className="py-5" id="centerImage" src="images/manoa-students.jpeg" alt="UH Manoa students." width="90%" />
-    </div>
-    <div>
-      <p>
-        Welcome to ClubConnect, the ultimate app for college students looking to discover new clubs and organizations on their university campus!
+    <Row>
+      <div className="justify-content-center">
+        <img className="py-5" id="centerImage" src="images/manoa-students.jpeg" alt="UH Manoa students." width="90%" />
+      </div>
+      <div>
 
-        Are you tired of attending the same events and meetings week after week? Do you want to branch out and explore all that your university has to offer? Look no further than ClubConnect. Our app is designed to connect you with new and
-        exciting clubs that match your interests and passions.
-
-        Whether you are a freshman looking to find your niche on campus, or a senior hoping to try something new before graduation, ClubConnect has something for everyone.
-
-        So what are you waiting for? Download ClubConnect today and start discovering all the amazing clubs and organizations that your university has to offer!
-      </p>
-    </div>
-    <div>
-      <Button variant="outline-primary" size="lg">Get Started</Button>
-    </div>
+        <h1>Welcome to Rio-Connect</h1>
+        <p>Rio-Connect is the ultimate app for college students looking to discover new clubs and organizations on their university campus!
+        </p>
+        <p>
+          Are you tired of attending the same events and meetings week after week? Do you want to branch out and explore all that your university has to offer? Look no further than Rio-Connect. Our app is designed to connect you with new
+          and
+          exciting clubs that match your interests and passions.
+        </p>
+        <p>
+          Whether you are a freshman looking to find your niche on campus, or a senior hoping to try something new before graduation, ClubConnect has something for everyone.
+        </p>
+        <p>
+          So what are you waiting for? Download ClubConnect today and start discovering all the amazing clubs and organizations that your university has to offer!
+        </p>
+      </div>
+      <a href="/signup">
+        <div className="d-grid gap-2 py-5">
+          <Button variant="outline-primary" size="lg">Get Started</Button>
+        </div>
+      </a>
+    </Row>
   </Container>
 );
 

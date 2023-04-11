70c032354b8a87d343186153c50af397b8dd89d4
Sean Sunoo <97641529+Ssunoo2@users.noreply.github.com>
2023-04-10 18:35:20 -1000

    mockup

--- app/imports/ui/pages/AddClub.jsx
+++ app/imports/ui/pages/AddClub.jsx
@@ -1,27 +1,24 @@
 import React from 'react';
-import { Card, Col, Container, Row } from 'react-bootstrap';
-import { ErrorsField, NumField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
+import { Col, Container, Image, Row } from 'react-bootstrap';
 
 // Create a schema to specify the structure of the data to appear in the form.
 /* Renders the AddStuff page for adding a document. */
 const AddClub = () => (
   <Container className="py-3">
     <Row className="justify-content-center">
-      <Col xs={5}>
-        <Col className="text-center"><h2>Create a New Club</h2></Col>
-        <Card>
-          <Card.Body>
-            {/*
+      <Col className="text-center"><h2>Create a New Club</h2></Col>
+      {/*
+            <Card>
+            <Card.Body>
             <TextField name="name" />
             <NumField name="quantity" />
             <SelectField name="condition" />
             <SubmitField value="Submit" />
             <ErrorsField />
+            </Card.Body>
+            </Card>
             */}
-
-          </Card.Body>
-        </Card>
-      </Col>
+      <Image src="../add-club-mockup.png" />
     </Row>
   </Container>
 );

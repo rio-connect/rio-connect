2df8c13bb8d28598ae544f9dc8d71c1060f3c538
nathan-getchel <ngetchel@hawaii.edu>
2023-04-13 10:19:43 -1000

    created profiles collection

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
 import { Clubs } from '../../api/club/Club';
+import { Profiles } from '../../api/profile/Profile';
 
 /* eslint-disable no-console */
 
@@ -29,5 +30,19 @@ if (Clubs.collection.find().count() === 0) {
   if (Meteor.settings.defaultClubs) {
     console.log('Creating default clubs.');
     Meteor.settings.defaultClubs.forEach(club => addClubs(club));
+  }
+}
+
+// Initialize the database with default profiles.
+const addProfiles = (profile) => {
+  console.log(`  Adding: ${profile.name}`);
+  Profiles.collection.insert(profile);
+};
+
+// Initialize the ProfilesCollection if empty.
+if (Profiles.collection.find().count() === 0) {
+  if (Meteor.settings.defaultProfiles) {
+    console.log('Creating default profiles.');
+    Meteor.settings.defaultProfiles.forEach(profile => addProfiles(profile));
   }
 }
--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { Stuffs } from '../../api/stuff/Stuff';
 import { Clubs } from '../../api/club/Club';
+import { Profiles } from '../../api/profile/Profile';
 
 // User-level publication.
 // If logged in, then publish documents owned by this user. Otherwise publish nothing.
@@ -37,6 +38,25 @@ Meteor.publish(Clubs.userPublicationName, function () {
 Meteor.publish(Clubs.adminPublicationName, function () {
   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
     return Clubs.collection.find();
+  }
+  return this.ready();
+});
+
+// User-level publication.
+// If logged in, then publish the profile belonging to this user. Otherwise publish nothing.
+Meteor.publish(Profiles.userPublicationName, function () {
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userId).username;
+    return Profiles.collection.find({ email: username });
+  }
+  return this.ready();
+});
+
+// Admin-level publication.
+// If logged in and with admin role, then publish all clubs from all users. Otherwise publish nothing.
+Meteor.publish(Profiles.adminPublicationName, function () {
+  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
+    return Profiles.collection.find();
   }
   return this.ready();
 });

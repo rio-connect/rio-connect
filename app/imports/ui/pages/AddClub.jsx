bd1d0c0660ef6a3da9d76f1751dfe81856920057
Sean Sunoo <97641529+Ssunoo2@users.noreply.github.com>
2023-04-12 18:57:55 -1000

    AddClub page updates

--- app/imports/ui/pages/AddClub.jsx
+++ app/imports/ui/pages/AddClub.jsx
@@ -5,11 +5,9 @@ import { Col, Container, Image, Row, Card, Form, Button } from 'react-bootstrap'
 /* Renders the AddStuff page for adding a document. */
 const AddClub = () => (
   <Container className="py-3">
-    <p>reference image</p>
-    <Image src="images/add-club-mockup.png" alt="mockup reference"/>
     <Container className="py-3">
       <Card id="add-club-page">
-        <Card.Header className="d-flex justify-content-center"><h1>Create a Club</h1></Card.Header>
+        <Card.Header className="d-flex justify-content-center py-3"><h1>Your Club</h1></Card.Header>
         <Row>
           <Col>
             <Image src="images/rio-connect-logo.png" className="img-thumbnail m-4 p-4" />
@@ -19,7 +17,7 @@ const AddClub = () => (
               <Form className="p-4">
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label><h2>Club Name: </h2></Form.Label>
-                  <Form.Control type="text" placeholder="Enter Club Name" />
+                  <Form.Control type="text" placeholder="Your club name..." />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label><h2>Club Type: </h2></Form.Label>
@@ -30,11 +28,11 @@ const AddClub = () => (
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label><h2>Contact Person: </h2></Form.Label>
-                  <Form.Control type="text" placeholder="Enter club's lead contact" />
+                  <Form.Control type="text" placeholder="John Doe" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label><h2>Contact Email: </h2></Form.Label>
-                  <Form.Control type="text" placeholder="Enter contact's email" />
+                  <Form.Control type="text" placeholder="john@foo.com" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label><h2>Club Image: </h2></Form.Label>
@@ -47,7 +45,7 @@ const AddClub = () => (
         <Row className="px-4">
           <Form.Group controlId="exampleForm.ControlTextarea1">
             <Form.Label placeholder="Lorem ipsum"><h2>Club Purpose: </h2></Form.Label>
-            <Form.Control as="textarea" rows={3} />
+            <Form.Control as="textarea" rows={5} />
           </Form.Group>
           <div className="d-grid gap-2 py-5">
             <Button variant="primary" size="lg"> Save </Button>

612946413f519cbc8037a72002d577868156e036
Sean Sunoo <97641529+Ssunoo2@users.noreply.github.com>
2023-04-12 17:35:05 -1000

    clubcard and clubcardtestpage

--- app/imports/ui/components/ClubCard.jsx
+++ app/imports/ui/components/ClubCard.jsx
@@ -1,44 +1,37 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { Card } from 'react-bootstrap';
+import { Card, Col, Container, Image, Row } from 'react-bootstrap';
 import CardHeader from 'react-bootstrap/CardHeader';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
-const ClubCard = ({ card }) => (
-  <Card>
-    <CardHeader>Club Name</CardHeader>
-    <Row>
-      <Col>
-        <Image>Club image</Image>
-      </Col>
-      <Col>
-        <div>
-          <h2> Club Type</h2>
-          <p>Club type</p>
-        </div>
-        <div>
-          <h2> Contact Person</h2>
-          <p>contact person</p>
-        </div>
-        <div>
-          <h2> Contact email</h2>
-          <p>club email</p>
-        </div>
-      </Col>
-    </Row>
-      <Link to={`/edit/${club._id}`}>Edit</Link>
-  <Card />
+const ClubCard = () => (
+  <Container className="py-3">
+    <Card>
+      <CardHeader className="d-flex justify-content-center"><h1>Club Name</h1></CardHeader>
+      <Row>
+        <Col>
+          <Image src="images/generic-user.png" className="img-thumbnail m-4 p-4" />
+        </Col>
+        <Col className="d-flex align-items-center flex-column">
+          <Row className="px-4 pt-4">
+            <h2> Club Type</h2>
+            <p>Club type</p>
+          </Row>
+          <Row className="px-4">
+            <h2> Contact Person</h2>
+            <p>contact person</p>
+          </Row>
+          <Row className="px-4">
+            <h2> Contact email</h2>
+            <p>club email</p>
+          </Row>
+        </Col>
+      </Row>
+      <Row className="px-4">
+        <h2>Club Purpose</h2>
+        <p>lorem Ipsum</p>
+      </Row>
+    </Card>
+  </Container>
 );
-
-// Require a document to be passed to this component.
-ClubCard.propTypes = {
-  stuff: PropTypes.shape({
-    name: PropTypes.string,
-    quantity: PropTypes.number,
-    condition: PropTypes.string,
-    _id: PropTypes.string,
-  }).isRequired,
-};
 
 export default ClubCard;
--- app/imports/ui/pages/ClubCardTestPage.jsx
+++ app/imports/ui/pages/ClubCardTestPage.jsx
@@ -1,37 +1,27 @@
 import React from 'react';
-import { Card, Col, Container, Image, Row } from 'react-bootstrap';
-import CardHeader from 'react-bootstrap/CardHeader';
+import { Col, Container, Row } from 'react-bootstrap';
+import ClubCard from '../components/ClubCard';
 
 // Create a schema to specify the structure of the data to appear in the form.
 /* Renders the AddStuff page for adding a document. */
-const AddClub = () => (
-  <Container className="py-3">
-    <Card>
-      <CardHeader>Club Name</CardHeader>
-      <Row>
-        <Col>
-          <Image>Club image</Image>
-        </Col>
-        <Col>
-          <div>
-            <h2> Club Type</h2>
-            <p>Club type</p>
-          </div>
-          <div>
-            <h2> Contact Person</h2>
-            <p>contact person</p>
-          </div>
-          <div>
-            <h2> Contact email</h2>
-            <p>club email</p>
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <h2>Club Purpose</h2>
-        <p>lorem Ipsum</p>
-      </Row>
-    </Card>
+const ClubCardTestPage = () => (
+  <Container>
+    <Row>
+      <Col>
+        <ClubCard />
+      </Col>
+      <Col>
+        <ClubCard />
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <ClubCard />
+      </Col>
+      <Col>
+        <ClubCard />
+      </Col>
+    </Row>
   </Container>
 );
-export default AddClub;
+export default ClubCardTestPage;

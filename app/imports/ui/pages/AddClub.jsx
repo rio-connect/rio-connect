76dea2079c63eba9f381e0db46a7af011af16b9b
Sean Sunoo <97641529+Ssunoo2@users.noreply.github.com>
2023-04-12 19:35:01 -1000

    Edit Club component and addclubpage

--- app/imports/ui/pages/AddClub.jsx
+++ app/imports/ui/pages/AddClub.jsx
@@ -1,25 +1,9 @@
 import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import EditClub from '../components/EditClub';
 
 // Create a schema to specify the structure of the data to appear in the form.
 /* Renders the AddStuff page for adding a document. */
 const AddClub = () => (
-  <Container className="py-3">
-    <Row className="justify-content-center">
-      <Col className="text-center"><h2>Create a New Club</h2></Col>
-      {/*
-            <Card>
-            <Card.Body>
-            <TextField name="name" />
-            <NumField name="quantity" />
-            <SelectField name="condition" />
-            <SubmitField value="Submit" />
-            <ErrorsField />
-            </Card.Body>
-            </Card>
-            */}
-      <Image src="images/add-club-mockup.png" />
-    </Row>
-  </Container>
+  <EditClub />
 );
 export default AddClub;

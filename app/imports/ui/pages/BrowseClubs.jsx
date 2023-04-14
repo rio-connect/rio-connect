9207a1888f979390f9f8f48b6f09046ce6f0c609
Jake Walker <122703805+jakeswalker@users.noreply.github.com>
2023-04-14 13:16:47 -1000

    Improve front-end of the Browse Clubs page

--- app/imports/ui/pages/BrowseClubs.jsx
+++ app/imports/ui/pages/BrowseClubs.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Container, Button, Row, Col } from 'react-bootstrap';
+import { PlusLg } from 'react-bootstrap-icons';
 import ClubFilter from '../components/ClubFilter';
 import ClubCard from '../components/ClubCard';
 
-/* Renders the AddStuff page for adding a document. */
 const BrowseClubs = () => (
   <Container fluid className="mx-auto px-0 ">
     <Container fluid id="browseSection">
-      <h1 className="text-center py-5">Find your club. Get connected</h1>
+      <h1 className="text-center py-5">Find your club. Get connected.</h1>
       <ClubFilter />
     </Container>
-    <Container id="get-started" className="mt-4 py-5 px-0">
-      <a href="addclub" className="d-grid text-decoration-none text-white">
-        <Button className="py-4" variant="primary" size="lg">Create a Club!</Button>
+    <Container className="mt-4 py-2 px-0">
+      <a href="addclub" className="d-grid text-decoration-none text-white justify-content-center">
+        <Button id="createClubBtn" className="border border-3 rounded-pill" variant="outline-dark" size="lg"><h4 className="pt-3"><PlusLg className="h1" />&nbsp;&nbsp;Create a Club!</h4></Button>
       </a>
+      <h4 className="mt-4 ms-4">20 clubs found:</h4>
     </Container>
     <Container>
       <Row>

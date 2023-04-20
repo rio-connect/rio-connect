dd391ce929577cda63983e63fb0556f57844287f
Sean Sunoo <97641529+Ssunoo2@users.noreply.github.com>
2023-04-20 13:38:01 -1000

    made methods and added method import to client and server side main.js

--- app/imports/ui/components/AddClubComponent.jsx
+++ app/imports/ui/components/AddClubComponent.jsx
@@ -4,47 +4,90 @@ import { Meteor } from 'meteor/meteor';
 import SimpleSchema from 'simpl-schema';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import swal from 'sweetalert';
+// import { _ } from 'meteor/underscore';
+import { useTracker } from 'meteor/react-meteor-data';
 import { AutoForm, ErrorsField, LongTextField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
+import LoadingSpinner from './LoadingSpinner';
 import { Clubs } from '../../api/club/Club';
+import { ProfileClubs } from '../../api/profile/ProfileClubs';
+import { ProfilesInterests } from '../../api/profile/ProfileInterests';
+import { Profiles } from '../../api/profile/Profile';
+import { Interests } from '../../api/interests/Interests';
+import { addClubMethod } from '../../startup/both/Methods';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 // const allInterests = _.pluck(Clubs.collection.find().fetch(), 'type');
-const allInterests = ['Academic/Professional', 'Ethic/Cultural', 'Fraternity/Sorority', 'Honorary Society', 'Leisure/Recreational', 'Political', 'Religious/Spiritual', 'Service', 'Sports/Leisure', 'Student Affairs'];
 // const allInterests = _.pluck(interests, 'type');
-const formSchema = new SimpleSchema({
+// const formSchema = new SimpleSchema({
+//   name: String,
+//   type: { label: '', type: Array, optional: true },
+//   'type.$': { type: String, allowedValues: allInterests },
+//   description: String,
+//   owner: String,
+//   ownerMail: String,
+//   image: String,
+// });
+const makeSchema = () => new SimpleSchema({
   name: String,
-  type: { label: '', type: Array, optional: true },
-  'type.$': { type: String, allowedValues: allInterests },
+  type: { label: 'Types', type: Array, optional: true },
+  'type.$': { type: String, allowedValues: ['Academic/Professional', 'Ethic/Cultural', 'Fraternity/Sorority', 'Honorary Society', 'Leisure/Recreational', 'Political', 'Religious/Spiritual', 'Service', 'Sports/Leisure', 'Student Affairs'] },
   description: String,
   owner: String,
   ownerMail: String,
   image: String,
 });
 
-const bridge = new SimpleSchema2Bridge(formSchema);
+// const AddClubComponent = () => {
+//   // On submit, insert the data.
+//   const submit = (data, formRef) => {
+//     const { name, type, description, ownerMail, image, members } = data;
+//     const owner = Meteor.user().username;
+//     Clubs.collection.insert(
+//       { name, type, description, owner, ownerMail, image, members },
+//       (error) => {
+//         if (error) {
+//           swal('Error', error.message, 'error');
+//         } else {
+//           swal('Success', 'Club added successfully', 'success');
+//           formRef.reset();
+//         }
+//       },
+//     );
+//   };
 
 const AddClubComponent = () => {
-  // On submit, insert the data.
-  const submit = (data, formRef) => {
-    const { name, type, description, ownerMail, image, members } = data;
-    const owner = Meteor.user().username;
-    Clubs.collection.insert(
-      { name, type, description, owner, ownerMail, image, members },
-      (error) => {
-        if (error) {
-          swal('Error', error.message, 'error');
-        } else {
-          swal('Success', 'Club added successfully', 'success');
-          formRef.reset();
-        }
-      },
-    );
-  };
-  const transform = (label) => ` ${label}`;
 
+  /* On submit, insert the data. */
+  const submit = (data, formRef) => {
+    Meteor.call(addClubMethod, data, (error) => {
+      if (error) {
+        swal('Error', error.message, 'error');
+      } else {
+        swal('Success', 'Project added successfully', 'success').then(() => formRef.reset());
+      }
+    });
+  };
+
+  const { ready } = useTracker(() => {
+    // Ensure that minimongo is populated with all collections prior to running render().
+    const sub1 = Meteor.subscribe(Interests.userPublicationName);
+    const sub2 = Meteor.subscribe(Profiles.userPublicationName);
+    const sub3 = Meteor.subscribe(ProfilesInterests.userPublicationName);
+    const sub4 = Meteor.subscribe(ProfileClubs.userPublicationName);
+    const sub5 = Meteor.subscribe(Clubs.userPublicationName);
+    return {
+      ready: sub1.ready() && sub2.ready() && sub3.ready() && sub4.ready() && sub5.ready(),
+      interests: Interests.collection.find().fetch(),
+    };
+  }, []);
+  // const allInterests = _.pluck(interests, 'name');
+  const allInterests = ['Academic/Professional', 'Ethic/Cultural', 'Fraternity/Sorority', 'Honorary Society', 'Leisure/Recreational', 'Political', 'Religious/Spiritual', 'Service', 'Sports/Leisure', 'Student Affairs'];
+  const formSchema = makeSchema(allInterests);
+  const bridge = new SimpleSchema2Bridge(formSchema);
+  const transform = (label) => ` ${label}`;
   // Render the form. Use Uniforms: https://github.com/vazco/uniforms
   let fRef = null;
-  return (
+  return ready ? (
     <Container className="py-3 gray-background">
       <Row className="justify-content-center">
         <Col>
@@ -94,7 +137,7 @@ const AddClubComponent = () => {
         </Col>
       </Row>
     </Container>
-  );
+  ) : <LoadingSpinner />;
 };
 
 export default AddClubComponent;
--- app/server/main.js
+++ app/server/main.js
@@ -1,3 +1,5 @@
 import '/imports/startup/server/Accounts';
 import '/imports/startup/server/Publications';
 import '/imports/startup/server/Mongo';
+// Get the Meteor methods.
+import '/imports/startup/both/Methods';

5983136c4487d77db8ec6b1b0383968f5f6532ce
montoyaoa <99383055+montoyaoa@users.noreply.github.com>
2023-04-22 19:49:13 -1000

    Added different credential levels
    Added credentials for a regular user, a club owner, and an admin.

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -6,7 +6,9 @@ import { navBar } from './navbar.component';
 /* global fixture:false, test:false */
 
 /** Credentials for one of the sample users defined in settings.development.json. */
-const credentials = { username: 'john@foo.com', password: 'changeme' };
+const regularUsercredentials = { username: 'emma@foo.com', password: 'changeme' };
+const clubOwnerCredentials = { username: 'john@foo.com', password: 'changeme' };
+const adminCredentials = { username: 'admin@foo.com', password: 'changeme' };
 
 fixture('rio-connect localhost test with default db')
   .page('http://localhost:3000');
@@ -17,8 +19,8 @@ test('Test that landing page shows up', async (testController) => {
 
 test('Test that signin and signout work', async (testController) => {
   await navBar.gotoSignInPage(testController);
-  await signinPage.signin(testController, credentials.username, credentials.password);
-  await navBar.isLoggedIn(testController, credentials.username);
+  await signinPage.signin(testController, clubOwnerCredentials.username, clubOwnerCredentials.password);
+  await navBar.isLoggedIn(testController, clubOwnerCredentials.username);
   await navBar.logout(testController);
   await signoutPage.isDisplayed(testController);
 });

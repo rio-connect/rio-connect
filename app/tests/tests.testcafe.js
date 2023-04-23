10f54b441006d67379aa2ed12853d62cc7cb05fb
montoyaoa <99383055+montoyaoa@users.noreply.github.com>
2023-04-22 20:28:50 -1000

    Added profile information to users
    Changed name to represent complete user abstraction. Added name and phone number fields.

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -6,10 +6,10 @@ import { navBar } from './navbar.component';
 /* global fixture:false, test:false */
 
 /** Credentials for one of the sample users defined in settings.development.json. */
-const regularUserCredentials = { username: 'emma@foo.com', password: 'changeme' };
-const clubOwnerCredentials = { username: 'john@foo.com', password: 'changeme' };
-const adminCredentials = { username: 'admin@foo.com', password: 'changeme' };
-const credentialsArray = [regularUserCredentials, clubOwnerCredentials, adminCredentials];
+const regularUser = { username: 'emma@foo.com', password: 'changeme', name: 'Emma Lee', phoneNo: '(808) 123-4567' };
+const clubOwner = { username: 'john@foo.com', password: 'changeme', name: 'John Smith', phoneNo: '(808) 555-5555' };
+const admin = { username: 'admin@foo.com', password: 'changeme', name: 'Admin', phoneNo: '(808) 555-5555' };
+const credentialsArray = [regularUser, clubOwner, admin];
 
 fixture('rio-connect localhost test with default db')
   .page('http://localhost:3000');
@@ -20,8 +20,8 @@ test('Test that landing page shows up', async (testController) => {
 
 test('Test that signin and signout work', async (testController) => {
   await navBar.gotoSignInPage(testController);
-  await signinPage.signin(testController, clubOwnerCredentials.username, clubOwnerCredentials.password);
-  await navBar.isLoggedIn(testController, clubOwnerCredentials.username);
+  await signinPage.signin(testController, clubOwner.username, clubOwner.password);
+  await navBar.isLoggedIn(testController, clubOwner.username);
   await navBar.logout(testController);
   await signoutPage.isDisplayed(testController);
 });

30e1b0538b49c82811f4b549d902e74a701302e3
Jake Walker <122703805+jakeswalker@users.noreply.github.com>
2023-04-25 11:48:48 -1000

    Increase wait time to 2 min

--- app/tests/landing.page.js
+++ app/tests/landing.page.js
@@ -8,8 +8,8 @@ class LandingPage {
 
   /** Asserts that this page is currently displayed. */
   async isDisplayed(testController) {
-    // This is first test to be run. Wait 90 seconds to avoid timeouts with GitHub Actions.
-    await testController.wait(90000).expect(this.pageSelector.exists).ok();
+    // This is first test to be run. Wait 120 seconds to avoid timeouts with GitHub Actions.
+    await testController.wait(120000).expect(this.pageSelector.exists).ok();
   }
 }
 

365e6c746a8ce765dbeef2691440412fe195b09d
montoyaoa <99383055+montoyaoa@users.noreply.github.com>
2023-04-28 22:27:07 -1000

    Break up EditClubPage
    Broke up EditClubPage into two components: EditClubInformation and EditClubRoster. The Page subscribes to the club to be edited and passes that information to the child components. Also implemented kick member logic to the club roster.

---
+++ app/imports/ui/components/EditClubInformation.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import swal from 'sweetalert';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import { Card, Col, Row } from 'react-bootstrap';
+import { AutoForm, ErrorsField, LongTextField, SubmitField, TextField, HiddenField } from 'uniforms-bootstrap5';
+import PropTypes from 'prop-types';
+import { Clubs } from '../../api/club/Club';
+
+// Use the Clubs schema for the form's fields.
+const bridge = new SimpleSchema2Bridge(Clubs.schema);
+
+const EditClubInformation = ({ club }) => {
+  // Callback function to submit a change to a club's information.
+  const submit = (data) => {
+    const { name, type, description, owner, ownerMail, members, image } = data;
+    Clubs.collection.update(
+      club._id,
+      { $set: { name, type, description, owner, ownerMail, members, image } },
+      (error) => (error ?
+        swal('Error', error.message, 'error') :
+        swal('Success', 'Club updated successfully', 'success')),
+    );
+  };
+
+  // Define the appearance of the component.
+  return (
+    <AutoForm schema={bridge} onSubmit={data => submit(data)} model={club}>
+      <Card>
+        <Card.Body>
+          <Row>
+            <Col>
+              <TextField id="edit-form-name" name="name" />
+            </Col>
+            <Col>
+              <TextField id="edit-form-image" name="image" />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <TextField id="edit-form-owner" name="owner" />
+            </Col>
+            <Col>
+              <TextField id="edit-form-mail" name="ownerMail" />
+            </Col>
+          </Row>
+          <LongTextField id="edit-form-description" name="description" />
+          <Row>
+            <TextField id="edit-form-type" name="type" />
+          </Row>
+          <SubmitField id="edit-form-submit" value="Submit" />
+          <ErrorsField />
+          <HiddenField name="members" />
+        </Card.Body>
+      </Card>
+    </AutoForm>
+  );
+};
+
+EditClubInformation.propTypes = {
+  club: PropTypes.shape({
+    name: PropTypes.string,
+    image: PropTypes.string,
+    description: PropTypes.string,
+    owner: PropTypes.string,
+    ownerMail: PropTypes.string,
+    type: PropTypes.string,
+    _id: PropTypes.string,
+  }).isRequired,
+};
+
+export default EditClubInformation;
--- app/imports/ui/pages/EditClubPage.jsx
+++ app/imports/ui/pages/EditClubPage.jsx
@@ -1,15 +1,53 @@
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
-import EditClub from '../components/EditClub';
+import { Col, Container, Row } from 'react-bootstrap';
+import { useParams } from 'react-router';
+import { useTracker } from 'meteor/react-meteor-data';
+import { Meteor } from 'meteor/meteor';
+import { Roles } from 'meteor/alanning:roles';
+import { Clubs } from '../../api/club/Club';
+import LoadingSpinner from '../components/LoadingSpinner';
+import EditClubInformation from '../components/EditClubInformation';
+import EditClubRoster from '../components/EditClubRoster';
 
-// Create a schema to specify the structure of the data to appear in the form.
-/* Renders the AddStuff page for adding a document. */
-const EditClubPage = () => (
+const EditClubPage = () => {
+  // Get the club ID from the URL.
+  const { _id } = useParams();
 
-  <Container>
-    <Row>
-      <EditClub />
-    </Row>
-  </Container>
-);
+  const { club, ready } = useTracker(() => {
+    // Subscribe to the ClubsCollection.
+    let subscription = null;
+    if (Roles.userIsInRole(Meteor.userId(), 'admin')) {
+      subscription = Meteor.subscribe(Clubs.adminPublicationName);
+    } else {
+      subscription = Meteor.subscribe(Clubs.userPublicationName);
+    }
+    // Determine if the subscription is ready.
+    const rdy = subscription.ready();
+    // Get the club to be edited.
+    const clubToBeEdited = Clubs.collection.findOne(_id);
+    return {
+      club: clubToBeEdited,
+      ready: rdy,
+    };
+  }, [_id]);
+
+  // Define the appearance of the page.
+  // Render the Edit Club page only when the subscription is ready.
+  return ready ? (
+    <Container className="py-3">
+      <Row className="justify-content-center">
+        <Col xs={10}>
+          <Col className="text-center">
+            <h2>Edit Club</h2>
+          </Col>
+          <EditClubInformation club={club} />
+          <Col className="text-center">
+            <h2>Club Roster</h2>
+          </Col>
+          <EditClubRoster club={club} />
+        </Col>
+      </Row>
+    </Container>
+  ) : <LoadingSpinner />;
+};
 export default EditClubPage;

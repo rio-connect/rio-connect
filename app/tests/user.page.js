4909c37c17ed161d1feb43bb8da2d29744ec2eaa
montoyaoa <99383055+montoyaoa@users.noreply.github.com>
2023-04-30 20:44:17 -1000

    Try reducing calls to other tests.
    This is a last-ditch effort to keep all test functionality before I start cutting tests.

--- app/tests/user.page.js
+++ app/tests/user.page.js
@@ -7,74 +7,16 @@ class UserPage {
     this.pageSelector = Selector(this.pageId);
   }
 
-  async canJoinLeaveClub(testController, user) {
-    console.log('performing test: this.verifyLeaveClub');
-    await this.verifyLeaveClub(testController, user);
-    console.log('performing test: this.verifyJoinClub');
-    await this.verifyJoinClub(testController, user);
-    console.log('performing test: this.correctClubMembershipInformation');
-    await this.correctClubMembershipInformation(testController, user);
-  }
-
-  async canEditProfileInformation(testController, user) {
-    console.log('performing test: this.editProfileInformation');
-    await this.editProfileInformation(testController, user);
-    console.log('performing test: navBar.gotoBrowseClubsPage');
-    await navBar.gotoBrowseClubsPage(testController);
-    console.log('performing test: navBar.gotoUserPage');
-    await navBar.gotoUserPage(testController);
-    console.log('performing test: this.verifyEditedProfileInformation');
-    await this.verifyEditedProfileInformation(testController, user);
-    console.log('performing test: this.restoreProfileInformation');
-    await this.restoreProfileInformation(testController, user);
-    console.log('performing test: this.correctProfileInformation');
-    await this.correctProfileInformation(testController, user);
-  }
-
   async validateUserPage(testController, user) {
+    /** Checks that this page is currently displayed. */
     console.log('performing test: this.isDisplayed');
-    await this.isDisplayed(testController, user);
+    await testController.expect(this.pageSelector.exists).ok(`Check that ${user.name} can see their UserPage`);
     console.log('performing test: this.correctProfileInformation');
     await this.correctProfileInformation(testController, user);
     console.log('performing test: this.correctClubMembershipInformation');
     await this.correctClubMembershipInformation(testController, user);
+    /** Checks that the user can edit all the clubs that they have permission to edit. */
     console.log('performing test: this.correctClubEditingInformation');
-    await this.correctClubEditingInformation(testController, user);
-  }
-
-  /** Checks that this page is currently displayed. */
-  async isDisplayed(testController, user) {
-    await testController.expect(this.pageSelector.exists).ok(`Check that ${user.name} can see their UserPage`);
-  }
-
-  /** Checks that the user's profile information is correct. */
-  async correctProfileInformation(testController, user) {
-    await testController.expect(Selector('#user-contact-info').exists).ok(`Check that ${user.name} can see the UserContactInfo component`);
-    console.log('In correctProfileInformation:');
-    console.log(await Selector('#user-name').value);
-    console.log(await Selector('#user-phone').value);
-    await testController.expect(Selector('#user-name').value).eql(user.name);
-    await testController.expect(Selector('#user-email').value).eql(user.username);
-    await testController.expect(Selector('#user-phone').value).eql(user.phoneNo);
-  }
-
-  /** Checks that the user has access to all the clubs they are a member of. */
-  async correctClubMembershipInformation(testController, user) {
-    await testController.expect(Selector('#user-club-list').exists).ok(`Check that ${user.name} can see the UserClubList component`);
-    /** Ensure that the number of UserClubCards match the number of clubs the user has joined. Admins have access to all 191 clubs. */
-    if (!user.isAdmin) {
-      await testController.expect(Selector('#user-club-card').count).eql(user.joinedClubs.length, `Check that non-admin user ${user.name} can see ${user.joinedClubs.length} UserClubCard components`);
-    } else {
-      await testController.expect(Selector('#user-club-card').count).eql(191);
-    }
-    /** Check that, for each club the user has joined, there is a UserClubCard with that club name. */
-    await Promise.all(user.joinedClubs.map(async (club) => {
-      await testController.expect(Selector('#user-club-card-name').withText(club).exists).ok(`Check that ${user.name} can see ${club} UserClubCard`);
-    }));
-  }
-
-  /** Checks that the user can edit all the clubs that they have permission to edit. */
-  async correctClubEditingInformation(testController, user) {
     if (user.canEditClubs) {
       // For each club the user can edit:
       await user.editableClubs.reduce(async (previousPromise, club) => {
@@ -92,51 +34,59 @@ class UserPage {
     }
   }
 
-  /** Edit the user's profile information to junk data. */
-  async editProfileInformation(testController, user) {
+  async canEditProfileInformation(testController, user) {
+    console.log('performing test: this.editProfileInformation');
+
+    /** Edit the user's profile information to junk data. */
     await testController.expect(Selector('#user-contact-info').exists).ok(`Check that ${user.name} can edit the UserContactInfo component`);
     console.log('In editProfileInformation:');
     console.log(await Selector('#user-name').value);
     console.log(await Selector('#user-phone').value);
     await testController.selectText(Selector('#user-name')).typeText(Selector('#user-name'), 'Abc123');
     await testController.selectText(Selector('#user-phone')).typeText(Selector('#user-phone'), '111-222-3333');
-
     await testController.click(Selector('#user-update-profile .btn'));
     await testController.click('.swal-button--confirm');
     console.log('In editProfileInformation:');
     console.log(await Selector('#user-name').value);
     console.log(await Selector('#user-phone').value);
-  }
+    console.log('performing test: navBar.gotoBrowseClubsPage');
 
-  /** Validate that the junk data is saved to the user's profile. */
-  async verifyEditedProfileInformation(testController, user) {
+    /** Navigate from and to the UserPage to make sure the values stick */
+    await navBar.gotoBrowseClubsPage(testController);
+    console.log('performing test: navBar.gotoUserPage');
+    await navBar.gotoUserPage(testController);
+
+    /** Validate that the junk data is saved to the user's profile. */
+    console.log('performing test: this.verifyEditedProfileInformation');
     await testController.expect(Selector('#user-contact-info').exists).ok(`Check that ${user.name} can edit the UserContactInfo component`);
     console.log('In verifyEditedProfileInformation:');
     console.log(await Selector('#user-name').value);
     console.log(await Selector('#user-phone').value);
     await testController.expect(Selector('#user-name').value).eql('Abc123');
     await testController.expect(Selector('#user-phone').value).eql('111-222-3333');
-  }
 
-  /** Restore the user's original profile information. */
-  async restoreProfileInformation(testController, user) {
+    /** Restore the user's original profile information. */
+    console.log('performing test: this.restoreProfileInformation');
     await testController.expect(Selector('#user-contact-info').exists).ok(`Check that ${user.name} can edit the UserContactInfo component`);
     console.log('In restoreProfileInformation:');
     console.log(await Selector('#user-name').value);
     console.log(await Selector('#user-phone').value);
     await testController.selectText(Selector('#user-name')).typeText(Selector('#user-name'), user.name);
     await testController.selectText(Selector('#user-phone')).typeText(Selector('#user-phone'), user.phoneNo);
-
     await testController.click(Selector('#user-update-profile .btn'));
     await testController.click('.swal-button--confirm');
     // Verify that the text boxes have been updated to have the correct value.
     console.log('In restoreProfileInformation:');
     console.log(await Selector('#user-name').value);
     console.log(await Selector('#user-phone').value);
+
+    console.log('performing test: this.correctProfileInformation');
+    await this.correctProfileInformation(testController, user);
   }
 
-  /** Leave a club. */
-  async verifyLeaveClub(testController, user) {
+  async canJoinLeaveClub(testController, user) {
+    /** Leave a club. */
+    console.log('performing test: this.verifyLeaveClub');
     if (user.isAdmin) {
       // An Admin user is not a member of any club, and thus should not be able to leave any club.
       await testController.expect(Selector('#leave-club-btn').exists).notOk('Ensure that Admin user cannot leave any club');
@@ -167,10 +117,8 @@ class UserPage {
       // Verify that the only clubs the user can see now are those that they own.
       await testController.expect(Selector('#user-club-card').count).eql(user.editableClubs.length, `Check that non-admin user ${user.name} can see ${user.editableClubs.length} UserClubCard components`);
     }
-  }
-
-  /** Rejoin all clubs that were left in the previous test. */
-  async verifyJoinClub(testController, user) {
+    /** Rejoin all clubs that were left in the previous test. */
+    console.log('performing test: this.verifyJoinClub');
     // Admins cannot join clubs.
     if (!user.isAdmin) {
       await navBar.gotoBrowseClubsPage(testController);
@@ -188,6 +136,34 @@ class UserPage {
       }, Promise.resolve());
       await navBar.gotoUserPage(testController);
     }
+    console.log('performing test: this.correctClubMembershipInformation');
+    await this.correctClubMembershipInformation(testController, user);
+  }
+
+  /** Checks that the user's profile information is correct. */
+  async correctProfileInformation(testController, user) {
+    await testController.expect(Selector('#user-contact-info').exists).ok(`Check that ${user.name} can see the UserContactInfo component`);
+    console.log('In correctProfileInformation:');
+    console.log(await Selector('#user-name').value);
+    console.log(await Selector('#user-phone').value);
+    await testController.expect(Selector('#user-name').value).eql(user.name);
+    await testController.expect(Selector('#user-email').value).eql(user.username);
+    await testController.expect(Selector('#user-phone').value).eql(user.phoneNo);
+  }
+
+  /** Checks that the user has access to all the clubs they are a member of. */
+  async correctClubMembershipInformation(testController, user) {
+    await testController.expect(Selector('#user-club-list').exists).ok(`Check that ${user.name} can see the UserClubList component`);
+    /** Ensure that the number of UserClubCards match the number of clubs the user has joined. Admins have access to all 191 clubs. */
+    if (!user.isAdmin) {
+      await testController.expect(Selector('#user-club-card').count).eql(user.joinedClubs.length, `Check that non-admin user ${user.name} can see ${user.joinedClubs.length} UserClubCard components`);
+    } else {
+      await testController.expect(Selector('#user-club-card').count).eql(191);
+    }
+    /** Check that, for each club the user has joined, there is a UserClubCard with that club name. */
+    await Promise.all(user.joinedClubs.map(async (club) => {
+      await testController.expect(Selector('#user-club-card-name').withText(club).exists).ok(`Check that ${user.name} can see ${club} UserClubCard`);
+    }));
   }
 }
 

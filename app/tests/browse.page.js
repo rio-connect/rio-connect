29bd9a40bd23bd687e8875cd7f49597a1524e0c5
Jake Walker <122703805+jakeswalker@users.noreply.github.com>
2023-04-30 23:11:15 -1000

    Implement further BrowseClubsPage testing

--- app/tests/browse.page.js
+++ app/tests/browse.page.js
@@ -13,22 +13,65 @@ class BrowsePage {
 
   /** Checks this page is displayed, then checks that filtering works. */
   async filter(testController) {
-    await this.isDisplayed(testController);
     // Select visualization
     const interestsSelector = Selector('#selectInterests div.form-check input');
-    // Show number of Clubs
-    await testController.click('#numClubs');
     // Select the fourth checkbox (Honorary Society)
     await testController.click(interestsSelector.nth(3));
     // Select the tenth checkbox (Student Affairs)
     await testController.click(interestsSelector.nth(9));
-    // Click "Apply"
+    // Click "Apply" button
     await testController.click('#selectInterestsApply input.btn.btn-primary');
     // Check that 15 cards are displayed.
     const cardCount = Selector('.card').count;
     await testController.expect(cardCount).eql(15);
-    // Show number of Clubs
-    await testController.click('#numClubs');
+  }
+
+  async clear(testController) {
+    // Click "Clear" button
+    await testController.click('#clearFilterBtn');
+    // Check that 191 cards are displayed.
+    const cardCount = Selector('.card').count;
+    await testController.expect(cardCount).eql(191);
+  }
+
+  async search(testController) {
+    // Type "anime" into search bar
+    await testController.selectText(Selector('#searchBar')).typeText(Selector('#searchBar'), 'anime');
+    // Click "Apply" button
+    await testController.click('#selectInterestsApply input.btn.btn-primary');
+    // Check that only a single card is displayed.
+    const cardCount = Selector('.card').count;
+    await testController.expect(cardCount).eql(1);
+  }
+
+  async joinClub(testController) {
+    // Select the first club
+    const firstClub = 'Academy of Creative Media Student Association';
+    const firstClubCard = Selector('#browse-club-card').withText(firstClub).parent();
+    // Click "Join Club" button
+    const joinClubButton = firstClubCard.find('#join-club-btn');
+    await testController.click(joinClubButton);
+  }
+
+  async leaveClub(testController) {
+    // Select the first club
+    const firstClub = 'Academy of Creative Media Student Association';
+    const firstClubCard = Selector('#browse-club-card').withText(firstClub).parent();
+    // Click "Leave Club" button
+    const leaveClubButton = firstClubCard.find('#leave-club-btn');
+    await testController.click(leaveClubButton);
+    await testController.click('.swal-button--confirm');
+    await testController.click('.swal-button--confirm');
+  }
+
+  async correctNumOfJoinedClubs(testController, user, numOfJoinedClubs) {
+    await testController.expect(Selector('#user-club-list').exists).ok();
+    await testController.expect(Selector('#user-club-card').count).eql(numOfJoinedClubs);
+  }
+
+  async createClubBtn(testController) {
+    // Click "Create a Club!" button
+    await testController.click('#createClubLink');
   }
 
   async verifyClub(testController, user, name) {
